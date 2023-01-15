af2e3f79e444037071ce7af762f3b362e420a1eb
Kang Wang <kangqiwang@outlook.com>
2023-01-15 20:35:31 +0000

    first commit

---
+++ datafeed/src/main/java/com/amber/data/houseuk/HouseukApplication.java
@@ -0,0 +1,12 @@
+package com.amber.data.houseuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseukApplication.class, args);
+	}
+}
---
+++ datafeed/src/test/java/com/amber/data/houseuk/HouseukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amber.data.houseuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
