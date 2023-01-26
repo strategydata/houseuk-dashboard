830ab720b0ea1bd1546ae335e1e60bf969634e8a
Kang Wang <kangqiwang@outlook.com>
2023-01-26 17:07:39 +0000

    update transaction

--- datafeed/src/main/java/com/amberData/datafeed/houseuk/pojo/Transaction.java
+++ datafeed/src/main/java/com/amberData/datafeed/houseuk/pojo/Transaction.java
@@ -1,14 +1,27 @@
 package com.amberData.datafeed.houseuk.pojo;
 
 import lombok.Data;
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Data
 public class Transaction {
+    /*
+     * the API reference is:
+     * https://landregistry.data.gov.uk/app/root/doc/ppd
+     * 
+     * example:
+     * http://landregistry.data.gov.uk/data/ppi/transaction/EED73E75-EA88-6AF3-E053-6C04A8C08ABA
+     */
+    
     private String id;
 
     private String transactionId;
     private String about;
-    private String transactionDate;
+    
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate transactionDate;
     private String pricePaid;
     private Boolean newBuild;
     private Enum<PropertyType> propertyType;
