475cd59cee0143b9ebcb9aa9e986d23252da6124
Kang Wang <kangqiwang@outlook.com>
2023-01-29 23:48:47 +0000

    change pom for dev

---
+++ datafeed-scheduler/src/main/java/com/amberdata/datafeed/scheduler/datafeedscheduler/config/SchedulerConfig.java
@@ -0,0 +1,15 @@
+// package main.java.com.amberdata.datafeed.scheduler.datafeedscheduler.config;
+
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Bean;
+
+// import main.java.com.amberdata.datafeed.scheduler.datafeedscheduler.houseapiwrapper.HouseApiWrapper;
+
+// @Configuration
+// public class SchedulerConfig{
+
+//     @Bean
+//     public HouseApiWrapper houseApiWrapper(){
+//         return new HouseApiWrapper();
+//     }
+// }
---
+++ datafeed-scheduler/src/main/java/com/amberdata/datafeed/scheduler/datafeedscheduler/sched/DataFeedSchedTask.java
@@ -0,0 +1,15 @@
+// package main.java.com.amberdata.datafeed.scheduler.datafeedscheduler.sched;
+
+// import org.springframework.scheduling.annotation.Scheduled;
+// import org.springframework.stereotype.Component;
+// import org.springframework.beans.factory.annotation.Autowired;
+
+// import main.java.com.amberdata.datafeed.scheduler.datafeedscheduler.houseapiwrapper.HouseApiWrapper;
+
+// @Component
+// public class DataFeedSchedTask {
+//     @Autowired
+//     private HouseApiWrapper houseApiWrapper;
+
+
+// }
