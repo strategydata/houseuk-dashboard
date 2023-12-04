87ba7dc2518f49db8b0ac461570817c693e876c8
kang wang <kangqiwang@outlook.com>
2023-12-04 19:24:55 +0000

    update extraction land registry uk

---
+++ extract/landRegistry/src/execute.py
@@ -0,0 +1,11 @@
+import logging
+import spark
+import sys
+from os import environ as env
+
+if __name__=="__main__":
+    logging.basicConfig(stream=sys.stdout,level=20)
+    config_dict = env.copy()
+    df= spark.read.format("csv").load("http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-complete.csv")
+    
+    
