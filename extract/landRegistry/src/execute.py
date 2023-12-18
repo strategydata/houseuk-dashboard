3315d0dba6b76fe312cf29273f951bdefd7324a5
kang wang <kangqiwang@outlook.com>
2023-12-18 09:13:28 +0000

    update house uk dashboard

--- extract/landRegistry/src/execute.py
+++ extract/landRegistry/src/execute.py
@@ -1,11 +1,102 @@
 import logging
+import json
 import pyspark
 import sys
 from os import environ as env
 
 if __name__=="__main__":
-    logging.basicConfig(stream=sys.stdout,level=20)
+    logging.basicConfig(stream=sys.stdout, level=20)
+
+    record_counts = {}
+
+    bamboo = BambooAPI(subdomain="gitlab")
+
     config_dict = env.copy()
-    df= spark.read.format("csv").load("http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-complete.csv")
+    snowflake_load_database = config_dict["SNOWFLAKE_LOAD_DATABASE"].strip()
+
+    snowflake_engine = snowflake_engine_factory(config_dict, "LOADER")
+
+    # Company Directory
+    logging.info("Getting latest employee directory.")
+
+    employees = bamboo.get_employee_directory()
+
+    record_counts["directory"] = len(employees)
+
+    with open("directory.json", "w") as outfile:
+        json.dump(employees, outfile)
+
+    snowflake_stage_load_copy_remove(
+        "directory.json",
+        f"{snowflake_load_database}.bamboohr.bamboohr_load",
+        f"{snowflake_load_database}.bamboohr.directory",
+        snowflake_engine,
+    )
+
+    # Tabular Data
+    tabular_data = dict(
+        compensation="compensation",
+        jobinfo="jobInfo",
+        employmentstatus="employmentStatus",
+        custombonus="customBonus",
+        customontargetearnings="customOnTargetEarnings",
+        customcurrencyconversion="customCurrencyConversion",
+    )
+
+    for key, value in tabular_data.items():
+        logging.info(f"Querying for {value} tabular data...")
+        data = bamboo.get_tabular_data(value)
+
+        record_counts[key] = len(data)
+
+        with open(f"{key}.json", "w") as outfile:
+            json.dump(data, outfile)
+
+        snowflake_stage_load_copy_remove(
+            f"{key}.json",
+            f"{snowflake_load_database}.bamboohr.bamboohr_load",
+            f"{snowflake_load_database}.bamboohr.{key}",
+            snowflake_engine,
+        )
+
+    # Custom Reports
+    report_mapping = dict(id_employee_number_mapping="498")
+
+    for key, value in report_mapping.items():
+        logging.info(f"Querying for report number {value} into table {key}...")
+        data = bamboo.get_report(value)
+
+        with open(f"{key}.json", "w") as outfile:
+            json.dump(data, outfile)
+
+        record_counts[key] = len(data["employees"])
+
+        snowflake_stage_load_copy_remove(
+            f"{key}.json",
+            f"{snowflake_load_database}.bamboohr.bamboohr_load",
+            f"{snowflake_load_database}.bamboohr.{key}",
+            snowflake_engine,
+        )
+
+    # Metadata
+    metadata_mapping = dict(meta_fields="fields")
+
+    for key, value in metadata_mapping.items():
+        logging.info(f"Getting metadata for fields...")
+        data = bamboo.get_metadata(value)
+
+        with open(f"{key}.json", "w") as outfile:
+            json.dump(data, outfile)
+
+        record_counts[key] = len(data)
+
+        snowflake_stage_load_copy_remove(
+            f"{key}.json",
+            f"{snowflake_load_database}.bamboohr.bamboohr_load",
+            f"{snowflake_load_database}.bamboohr.{key}",
+            snowflake_engine,
+        )
+
+    push_to_xcom_file(record_counts)
     
     
---
+++ extract/tests/test_rightmove.py
@@ -0,0 +1,115 @@
+
+import pandas as pd
+import pytest
+
+from rightmove.src.api import Rightmove
+
+base_url = "https://www.rightmove.co.uk/"
+required_columns = {"address", "agent_url", "number_bedrooms", "postcode", "price", "search_date", "type", "url"}
+
+
+def test_sale_residential():
+    """Test a search on residential properties for sale."""
+    url = f"{base_url}property-for-sale/find.html?searchType=SALE&locationIdentifier=REGION%5E94346&insId=1"
+    rm = Rightmove(url)
+    assert isinstance(rm.average_price, float)
+    assert isinstance(rm.get_results, pd.DataFrame)
+    assert required_columns.issubset(set(rm.get_results.columns))
+    assert len(rm.get_results) > 0
+    assert isinstance(rm.page_count, int)
+    assert rm.rent_or_sale == "sale"
+    assert isinstance(rm.results_count, int)
+    assert isinstance(rm.results_count_display, int)
+    assert url == rm.url
+    df = rm.summary()
+    assert isinstance(df, pd.DataFrame)
+    assert {"number_bedrooms", "count", "mean"}.issubset(set(df.columns))
+    assert len(df) > 0
+    for c in required_columns:
+        df = rm.summary(by=c)
+        assert isinstance(df, pd.DataFrame)
+        assert {c, "count", "mean"}.issubset(set(df.columns))
+        assert len(df) > 0
+
+
+def test_rent_residential():
+    """Test a search on residential properties for rent."""
+    url = f"{base_url}property-to-rent/find.html?searchType=RENT&locationIdentifier=REGION%5E94346"
+    rm = Rightmove(url)
+    assert isinstance(rm.average_price, float)
+    assert isinstance(rm.get_results, pd.DataFrame)
+    assert required_columns.issubset(set(rm.get_results.columns))
+    assert len(rm.get_results) > 0
+    assert isinstance(rm.page_count, int)
+    assert rm.rent_or_sale == "rent"
+    assert isinstance(rm.results_count, int)
+    assert isinstance(rm.results_count_display, int)
+    assert url == rm.url
+    df = rm.summary()
+    assert isinstance(df, pd.DataFrame)
+    assert {"number_bedrooms", "count", "mean"}.issubset(set(df.columns))
+    assert len(df) > 0
+    for c in required_columns:
+        df = rm.summary(by=c)
+        assert isinstance(df, pd.DataFrame)
+        assert {c, "count", "mean"}.issubset(set(df.columns))
+        assert len(df) > 0
+
+
+def test_sale_commercial():
+    """Test a search on commercial properties for sale."""
+    url = f"{base_url}commercial-property-for-sale/find.html?searchType=SALE&locationIdentifier=REGION%5E70417"
+    rm = Rightmove(url)
+    assert isinstance(rm.average_price, float)
+    assert isinstance(rm.get_results, pd.DataFrame)
+    assert required_columns.issubset(set(rm.get_results.columns))
+    assert len(rm.get_results) > 0
+    assert isinstance(rm.page_count, int)
+    assert rm.rent_or_sale == "sale-commercial"
+    assert isinstance(rm.results_count, int)
+    assert isinstance(rm.results_count_display, int)
+    assert url == rm.url
+    df = rm.summary()
+    assert isinstance(df, pd.DataFrame)
+    assert {"type", "count", "mean"}.issubset(set(df.columns))
+    assert len(df) > 0
+    for c in required_columns:
+        if c == "number_bedrooms":
+            continue
+        df = rm.summary(by=c)
+        assert isinstance(df, pd.DataFrame)
+        assert {c, "count", "mean"}.issubset(set(df.columns))
+        assert len(df) > 0
+
+
+def test_rent_commercial():
+    """Test a search on commercial properties for rent."""
+    url = f"{base_url}commercial-property-to-let/find.html?searchType=RENT&locationIdentifier=REGION%5E70417"
+    rm = Rightmove(url)
+    assert isinstance(rm.average_price, float)
+    assert isinstance(rm.get_results, pd.DataFrame)
+    assert required_columns.issubset(set(rm.get_results.columns))
+    assert len(rm.get_results) > 0
+    assert isinstance(rm.page_count, int)
+    assert rm.rent_or_sale == "rent-commercial"
+    assert isinstance(rm.results_count, int)
+    assert isinstance(rm.results_count_display, int)
+    assert url == rm.url
+    df = rm.summary()
+    assert isinstance(df, pd.DataFrame)
+    assert {"type", "count", "mean"}.issubset(set(df.columns))
+    assert len(df) > 0
+    for c in required_columns:
+        if c == "number_bedrooms":
+            continue
+        df = rm.summary(by=c)
+        assert isinstance(df, pd.DataFrame)
+        assert {c, "count", "mean"}.issubset(set(df.columns))
+        assert len(df) > 0
+
+
+def test_bad_url():
+    """Test a bad URL raises a value error."""
+    bad_url = "https://www.rightmove.co.uk/property"
+    with pytest.raises(ValueError):
+        _ = Rightmove(bad_url)
