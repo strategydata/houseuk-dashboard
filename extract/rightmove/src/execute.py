aee9ac4174981007f1c25b4e78ea1f16ddbf0618
kang wang <kangqiwang@outlook.com>
2024-05-14 00:43:30 +0100

    update execute with connection timeout #1

--- extract/rightmove/src/execute.py
+++ extract/rightmove/src/execute.py
@@ -7,8 +7,10 @@ from api import Rightmove
 
 if __name__=="__main__":
     logging.basicConfig(stream=sys.stdout,level=20)
-    rm = Rightmove("https://www.rightmove.co.uk/property-for-sale/find.html?searchType=SALE&locationIdentifier=REGION%5E1195&insId=1")
-    logging.info("Getting lastest house directory")
-    # rm.get_results.to_csv("out.csv")
-    print(rm.average_price)
-    print(rm.median_price)
+    # greystone, sheffield
+    rm = Rightmove("https://www.rightmove.co.uk/property-for-sale/find.html?searchType=SALE&locationIdentifier=OUTCODE%5E2250&insId=1&sortType=6")
+    logging.info("Getting lastest house records")
+    rm.get_results.to_json("rightmove_out.json")
+    print(rm.results_count)
+    logging.info("Getting "+str(rm.results_count)+" house")
+    
