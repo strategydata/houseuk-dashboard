3d99b160b606996f8d53d279ed6300ab4fdecc90
Kang Wang <kangwang@fuildone.com>
2025-07-01 15:59:31 +0100

    fix: Update MydomainSpider to improve property data extraction and pagination handling

--- extract/rightmove/rightmove/spiders/mydomain.py
+++ extract/rightmove/rightmove/spiders/mydomain.py
@@ -1,6 +1,5 @@
 import scrapy
 from scrapy.spiders import SitemapSpider
-from scrapy_selenium import SeleniumRequest
 import logging
 
 from rightmove.items import RightmoveItem
@@ -17,7 +16,6 @@ class MydomainSpider(SitemapSpider):
         (r"\/property-for-sale\/([a-zA-Z]+\d+)+\.html", 'parse_for_sale'),
         (r"\/property-to-rent\/([a-zA-Z]+\d+)+\.html", 'parse_to_rent'),
     ]
-    
     index_number = 0
     increment = 24  # Number of items to increment for pagination
     def parse_for_sale(self, response):
@@ -28,37 +26,34 @@ class MydomainSpider(SitemapSpider):
             'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
             'Connection': 'keep-alive',
             'Host': 'www.rightmove.co.uk',
-            
         }
-        items = RightmoveItem()
-        homes = response.css("#l-searchResults").getall()
+        
+        homes = response.css("[class^=\"PropertyCard_propertyCardContainer_\"]")
         for home in homes:
-            items["url"]=f"https://www.rightmove.co.uk/"+home.css("a.propertyCard-link::attr(href)").get()
+            items = RightmoveItem()
+            items["url"]=f"https://www.rightmove.co.uk"+home.css("a.propertyCard-link::attr(href)").get()
             items["price"] = home.css("[class^=\"PropertyPrice_price_\"]::text").get()
             items["title"]=home.xpath("//address/text()").get()
-            items["date_added"]=home.css("[class^=\"MarketedBy_joinedText_\"::text]").get()
-            items["property_type"] = home.css("[class^=\"PropertyInformation_propertyType_\"::text]").get()
-            items["bedrooms"] = home.css("[class^=\"PropertyInformation_bedroomsCount_\"::text]").get()
-            items["bathooms"] = home.css("[class^=\"PropertyInformation_bathContainer_\"::text]").get()
-            items["phone"] = home.css("[class^=\"CallAgent_test_\"::text]").get()
-            items["address"]= home.css("[class^=\"PropertyAddress_address_\"::text]").get()
-            items["summary"]= home.css("[class^=\"PropertyCardSummary_summary_\"::text]").get()
-            items["email"]= home.css("[class^=\"Contact_emailLink_\"::attr(href)]").get()
-        # prices = response.css("div.PropertyCard_propertyCardContainer__VSRSA > div:nth-of-type(1) > div:nth-of-type(2)::text").get()
+            items["date_added"]=home.css("[class^=\"MarketedBy_joinedText_\"]::text").get()
+            items["property_type"] = home.css("[class^=\"PropertyInformation_propertyType_\"]::text").get()
+            items["bedrooms"] = home.css("[class^=\"PropertyInformation_bedroomsCount_\"]::text").get()
+            items["bathooms"] = home.css("[class^=\"PropertyInformation_bathContainer_\"] span::text").get()
+            items["phone"] = home.css("[class^=\"CallAgent_test_\"] > a:nth-child(2) > span:nth-child(1)::text").get()
+            items["address"]= home.css("[class^=\"PropertyAddress_address_\"]::text").get()
+            items["summary"]= home.css("[class^=\"PropertyCardSummary_summary_\"]::text").get()
+            items["email"]= home.css("[class^=\"Contact_emailLink_\"]::attr(href)").get()
+            yield items
         next_page = f"{response.url}?index={self.index_number}"
-        
-        yield scrapy.Request(
-            method='GET',
-            url=next_page,
-            headers=headers,
-            callback=self.parse_property
-        )
+        logger.debug(f"Next page URL: {next_page}")
+        if self.index_number < 1100:
+            self.index_number += self.increment
+            yield scrapy.Request(
+                method='GET',
+                url=next_page,
+                headers=headers,
+                callback=self.parse
+            )
     
     def parse_to_rent(self, response):
         # Extract the outcode from the URL
         pass
-    
-    def parse_property(self, response):
-        
-        item = RightmoveItem()
-        
