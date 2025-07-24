2aabd1f88bee270a4de9a04928550d74f9529215
kangqiwang <kangqiwang@outlook.com>
2025-07-24 22:31:09 +0100

    #8 commit changes , after running per-commit

--- extract/rightmove/rightmove/spiders/rightmove.py
+++ extract/rightmove/rightmove/spiders/rightmove.py
@@ -11,8 +11,9 @@ logger = logging.getLogger(__name__)
 
 class RightmoveSpider(scrapy.spiders.SitemapSpider):
     """
-    rightmove 
+    rightmove
     """
+
     name = "rightmove"
     sitemap_urls = ["https://www.rightmove.co.uk/sitemap.xml"]
     sitemap_rules = [
@@ -20,6 +21,7 @@ class RightmoveSpider(scrapy.spiders.SitemapSpider):
         (r"\/property-to-rent\/([a-zA-Z]+\d+)+\.html", "parse"),
     ]
     index_number = 0
+
     # increment = 24  # Number of items to increment for pagination
     def parse(self, response):
         # Extract the outcode from the URL
@@ -30,20 +32,18 @@ class RightmoveSpider(scrapy.spiders.SitemapSpider):
             "Connection": "keep-alive",
             "Host": "www.rightmove.co.uk",
         }
-        let_or_sales="sales"
+        let_or_sales = "sales"
         if "property-to-rent" in response.url:
-            let_or_sales="rent"
+            let_or_sales = "rent"
         homes = response.css('[class^="PropertyCard_propertyCardContainer_"]')
         if not homes:
             logger.debug(f"Ignoring no items response for URL: {response.url}")
             return
         for home in homes:
             items = RightmoveItem()
-            items["url"] = (
-                home.css("a.propertyCard-link::attr(href)").get()
-            )
+            items["url"] = home.css("a.propertyCard-link::attr(href)").get()
             items["price"] = home.css('[class^="PropertyPrice_price_"]::text').get()
-            
+
             items["title"] = home.xpath("//address/text()").get()
             items["date_added"] = home.css(
                 '[class^="MarketedBy_joinedText_"]::text'
@@ -68,15 +68,15 @@ class RightmoveSpider(scrapy.spiders.SitemapSpider):
             ).get()
             items["email"] = home.css('[class^="Contact_emailLink_"]::attr(href)').get()
             items["catalog_url"] = response.url
-            items["let_or_sales"]=let_or_sales
+            items["let_or_sales"] = let_or_sales
             yield items
         total = int(
             response.css('[class^="ResultsCount_resultsCount_"] p span::text').get()
         )
         logger.debug(f"Total properties found: {total}")
-        self.index_number=self.index_number+24
-        next_page = update_param(response.url, "index",self.index_number )
-        
+        self.index_number = self.index_number + 24
+        next_page = update_param(response.url, "index", self.index_number)
+
         # next_page = f"{response.url}?index={self.index_number}"
         logger.debug(f"Next page URL: {next_page}")
         logger.debug(f"Next self.index_number: {self.index_number}")
