daf3486bc2c0ee553b62d19eef826af1e1d33667
kangqiwang <kangqiwang@outlook.com>
2025-08-05 11:06:26 +0100

    update files based on pre-commit changes

--- house/extract/tests/test_langRegistry.py
+++ house/extract/tests/test_langRegistry.py
@@ -4,6 +4,7 @@ import logging
 
 from house.extract.landRegistry.langRegistry import download_and_save_parquet
 
+
 @pytest.mark.parametrize(
     "complete,expected_url,expected_filename,df_mock",
     [
@@ -12,41 +13,47 @@ from house.extract.landRegistry.langRegistry import download_and_save_parquet
             "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-complete.csv",
             "landRegistry_complete.parquet",
             pd.DataFrame({"col1": [1, 2], "col2": ["a", "b"]}),
-            id="happy_path_complete_true"
+            id="happy_path_complete_true",
         ),
         pytest.param(
             False,
             "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-monthly-update-new-version.csv",
             "landRegistry_monthly.parquet",
             pd.DataFrame({"col1": [], "col2": []}),
-            id="happy_path_complete_false_empty_df"
+            id="happy_path_complete_false_empty_df",
         ),
         pytest.param(
             True,
             "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-complete.csv",
             "landRegistry_complete.parquet",
             pd.DataFrame({"col1": [None, None]}),
-            id="edge_case_null_values"
+            id="edge_case_null_values",
         ),
-    ]
+    ],
 )
-def test_download_and_save_parquet_happy_and_edge(monkeypatch, complete, expected_url, expected_filename, df_mock):
-    
+def test_download_and_save_parquet_happy_and_edge(
+    monkeypatch, complete, expected_url, expected_filename, df_mock
+):
     # Arrange
     class DummyDF(pd.DataFrame):
         def to_parquet(self, fname, **kwargs):
             self._to_parquet_called = True
             self._to_parquet_args = (fname, kwargs)
+
     dummy_df = DummyDF(df_mock)
     read_csv_called = {}
+
     def mock_read_csv(url, encoding=None):
         read_csv_called["url"] = url
         read_csv_called["encoding"] = encoding
         return dummy_df
+
     monkeypatch.setattr(pd, "read_csv", mock_read_csv)
     logs = []
+
     def mock_info(msg):
         logs.append(msg)
+
     monkeypatch.setattr(logging, "info", mock_info)
 
     # Act
