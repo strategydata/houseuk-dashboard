643effe3ff4d855d281c84fa9dcbdc0e6311dc45
kangqiwang <kangqiwang@outlook.com>
2025-08-07 11:03:27 +0100

    update #47 change it to mongo

--- house/extract/rightmove/rightmove/pipelines.py
+++ house/extract/rightmove/rightmove/pipelines.py
@@ -6,7 +6,25 @@
 
 # useful for handling different item types with a single interface
 
+import pymongo
+
 
 class RightmovePipeline:
+    def __init__(self):
+        self.client = pymongo.MongoClient(host="localhost", port=27017)
+        self.collection = self.client["rightmove"]["properties"]
+
     def process_item(self, item, spider):
+        query = {
+            "_id": item["_id"],
+            "price": item["price"],
+        }
+        if self.collection.find_one(query):
+            # If the item already exists, update it
+            spider.logger.info(f"Duplicate item found: {item} — skipping.")
+        else:
+            self.collection.insert_one(dict(item))
         return item
+
+    def close_spider(self, spider):
+        self.client.close()
